56d0a85af687a6956cefc1d4c56112076d4956d9
GABRIEL PORRAS PÉREZ <gabporper@alu.edu.gva.es>
2023-12-01 14:52:55 +0100

    Cambios

---
+++ Comida/MainWindowVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comida
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<Plato> listaPlatos;
+        public ObservableCollection<Plato> ListaPlatos
+        {
+            get { return listaPlatos; }
+            set
+            {
+                listaPlatos = value;
+                NotifyPropertyChanged("ListaPlatos");
+            }
+        }      
+
+        private Plato platoSeleccionado;      
+        public Plato PlatoSeleccionado
+        {
+            get { return platoSeleccionado; }
+            set
+            {
+                platoSeleccionado = value;
+                NotifyPropertyChanged("PlatoSeleccionado");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            ListaPlatos = Plato.GetSamples("C:/Users/alumno/source/repos/Comida/Comida/recursos/");
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Declarar método para invocar el evento.
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
